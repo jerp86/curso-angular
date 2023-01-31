cdb248ab80c9a332826fffc53a3d84ebd9916b80
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-31 20:24:31 -0300

    create interface

---
+++ src/app/shared/models/movie.ts
@@ -0,0 +1,10 @@
+export interface Movie {
+  id?: number
+  title: string
+  imageUrl?: string
+  releaseDate: Date
+  description?: string
+  note: number
+  imdbUrl?: string
+  movieGenre: string
+}
