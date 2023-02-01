387fb1c117e00e584c84fa48b2ab58978fbe69f6
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-31 21:29:14 -0300

    fix colorSuccessButton

--- src/app/shared/components/alert/alert.component.ts
+++ src/app/shared/components/alert/alert.component.ts
@@ -19,7 +19,7 @@ export class AlertComponent implements OnInit {
     this.alert.description = this.data?.description || 'Seu registro foi salvo com sucesso!'
     this.alert.successButton = this.data?.successButton || 'OK'
     this.alert.cancelButton = this.data?.cancelButton || 'Cancelar'
-    this.alert.colorSuccessButton = this.data?.colorSuccessButton || 'primary'
+    this.alert.colorSuccessButton = this.data?.colorSuccessButton || 'accent'
     this.alert.colorCancelButton = this.data?.colorCancelButton || 'warn'
     this.alert.hasCloseButton = this.data?.hasCloseButton || false
   }
