a0217a208bcfe15a70cdece34d2b4fd52b4a3483
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-01 11:13:36 -0300

    add service

--- src/app/movies/movie-list/movie-list.component.ts
+++ src/app/movies/movie-list/movie-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from 'src/app/core/movies.service';
+import { Movie } from 'src/app/shared/models/movie';
 
 @Component({
   selector: 'dio-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.scss']
 })
-export class MovieListComponent {
+export class MovieListComponent implements OnInit {
+  movies = [] as Movie[]
+
+  constructor(private moviesService: MoviesService) {}
+
+  ngOnInit(): void {
+    this.moviesService.list().subscribe(movies => this.movies = movies)
+  }
+
   open() {}
 }
