b8364f4b7d961a8e556ef6ef4465b832756dbc76
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-31 21:18:56 -0300

    fix props with Alert interface

--- src/app/shared/components/alert/alert.component.ts
+++ src/app/shared/components/alert/alert.component.ts
@@ -1,38 +1,25 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-
-interface AlertProps {
-  title?: string
-  description?: string
-  successButton?: string
-  cancelButton?: string
-  colorButton?: string
-  hasCloseButton?: boolean
-}
+import { Alert } from '../../models/alert';
 
 @Component({
   selector: 'dio-alert',
   templateUrl: './alert.component.html',
 })
 export class AlertComponent implements OnInit {
-  title?: string
-  description?: string
-  successButton?: string
-  cancelButton?: string
-  colorButton?: string
-  hasCloseButton?: boolean
+  alert = {} as Alert
 
   constructor(
     public dialogRef: MatDialogRef<AlertComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: AlertProps,
+    @Inject(MAT_DIALOG_DATA) public data: Alert,
   ) {}
 
   ngOnInit(): void {
-    this.title = this.data?.title || 'Sucesso!'
-    this.description = this.data?.description || 'Seu registro foi salvo com sucesso!'
-    this.successButton = this.data?.successButton || 'OK'
-    this.cancelButton = this.data?.cancelButton || 'Cancelar'
-    this.colorButton = this.data?.colorButton || 'primary'
-    this.hasCloseButton = this.data?.hasCloseButton || false
+    this.alert.title = this.data?.title || 'Sucesso!'
+    this.alert.description = this.data?.description || 'Seu registro foi salvo com sucesso!'
+    this.alert.successButton = this.data?.successButton || 'OK'
+    this.alert.cancelButton = this.data?.cancelButton || 'Cancelar'
+    this.alert.colorButton = this.data?.colorButton || 'primary'
+    this.alert.hasCloseButton = this.data?.hasCloseButton || false
   }
 }
