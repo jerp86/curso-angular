ef1f85fc760303852c3425516c7e2333fc560a2a
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-02 01:16:36 -0300

    create component

---
+++ src/app/movies/movie-view/movie-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dio-movie-view',
+  templateUrl: './movie-view.component.html',
+  styleUrls: ['./movie-view.component.scss']
+})
+export class MovieViewComponent {
+
+}
--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -5,13 +5,15 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 import { MovieRegistrationComponent } from './movie-registration/movie-registration.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieViewComponent } from './movie-view/movie-view.component';
 import { MaterialModule } from '../shared/material/material.module';
 import { FieldsModule } from '../shared/components/fields/fields.module';
 
 @NgModule({
   declarations: [
+    MovieListComponent,
     MovieRegistrationComponent,
-    MovieListComponent
+    MovieViewComponent
   ],
   imports: [
     CommonModule,
