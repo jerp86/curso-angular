d497c58ce982c2d0bfe10d1e2c2b63219299f3d8
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-02 01:27:55 -0300

    add service

--- src/app/movies/movie-view/movie-view.component.ts
+++ src/app/movies/movie-view/movie-view.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MoviesService } from 'src/app/core/movies.service';
+import { Movie } from 'src/app/shared/models/movie';
 
 @Component({
   selector: 'dio-movie-view',
   templateUrl: './movie-view.component.html',
   styleUrls: ['./movie-view.component.scss']
 })
-export class MovieViewComponent {
+export class MovieViewComponent implements OnInit {
+  movie = {} as Movie
 
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private moviesService: MoviesService
+  ) {}
+
+  #getMovie(id: number) {
+    this.moviesService.getById(id).subscribe(movie => this.movie = movie)
+  }
+
+  ngOnInit(): void {
+    this.#getMovie(this.activateRoute.snapshot.params['id'])
+  }
 }
