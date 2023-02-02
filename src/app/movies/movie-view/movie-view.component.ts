e9a137062c4c3ade9d649338469b645db307a404
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-02 02:09:37 -0300

    create method deleteMovie

--- src/app/movies/movie-view/movie-view.component.ts
+++ src/app/movies/movie-view/movie-view.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MoviesService } from 'src/app/core/movies.service';
+import { AlertComponent } from 'src/app/shared/components/alert/alert.component';
+import { Alert } from 'src/app/shared/models/alert';
 import { Movie } from 'src/app/shared/models/movie';
 
 @Component({
@@ -15,8 +18,10 @@ export class MovieViewComponent implements OnInit {
   id = 0
 
   constructor(
+    public dialog: MatDialog,
     private activateRoute: ActivatedRoute,
-    private moviesService: MoviesService
+    private moviesService: MoviesService,
+    private router: Router
   ) {}
 
   #getMovie() {
@@ -26,5 +31,26 @@ export class MovieViewComponent implements OnInit {
   ngOnInit(): void {
     this.id = this.activateRoute.snapshot.params['id']
     this.#getMovie()
+  }
+
+  deleteMovie(): void {
+    const config = {
+      data: {
+        titulo: 'Você tem certeza que deseja excluir?',
+        descricao: 'Caso você tenha certceza que deseja excluir, clique no botão OK',
+        corBtnCancelar: 'primary',
+        corBtnSucesso: 'warn',
+        possuirBtnFechar: true
+      } as Alert
+    };
+    const dialogRef = this.dialog.open(AlertComponent, config);
+
+    dialogRef.afterClosed().subscribe((option: boolean) => {
+      if (option) {
+        this.moviesService
+          .deleteById(this.id)
+          .subscribe(() => this.router.navigateByUrl('/movies'));
+      }
+    });
   }
 }
