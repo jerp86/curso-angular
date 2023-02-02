e813a1800f5d354e2712934caaa76d1a409fb0a2
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-01 22:51:30 -0300

    alter props

--- src/app/core/movies.service.ts
+++ src/app/core/movies.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { GenericField } from '../shared/models/generic-field';
 import { Movie } from '../shared/models/movie';
 
 const url = 'http://localhost:3333/movies/'
@@ -15,20 +16,20 @@ export class MoviesService {
     return this.http.post<Movie>(url, movie)
   }
 
-  list(page: number, limit: number, searchText?: string, searchGenre?: string): Observable<Movie[]> {
+  list(page?: number, limit?: number, searchText?: string, searchField?: GenericField): Observable<Movie[]> {
     let httpParams = new HttpParams()
     httpParams = httpParams
       .set('_sort', 'id')
       .set('_order', 'desc')
-      .set('_page', page.toString())
-      .set('_limit', limit.toString())
+      .set('_page', page ? page.toString() : '')
+      .set('_limit', limit ? limit.toString() : '')
 
     if (searchText) {
       httpParams = httpParams.set('q', searchText)
     }
 
-    if (searchGenre) {
-      httpParams = httpParams.set('movieGenre', searchGenre)
+    if (searchField) {
+      httpParams = httpParams.set(searchField.type, searchField.value)
     }
 
     return this.http.get<Movie[]>(url, { params: httpParams })
